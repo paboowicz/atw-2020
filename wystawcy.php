47af6c459250b35eb3ee7d3a52d1cdab885e23ba
paboowicz <paboowicz@gmail.com>
2020-01-22 00:02:04 +0100

    usuniecie slidera

--- wystawcy.php
+++ wystawcy.php
@@ -1,7 +1,7 @@
 <?php require_once('header.php') ?>
 
 
-<?php require_once('slider.php') ?>
+<!--<?php require_once('slider.php') ?>  -->
     
     	
 <section id="sponsors" class="sponsors_3 section-padding  secondary-bg sponsors-index">
