6ef214b155152acea5e2ef0c44b2e5ec1f8368e3
rmpgitDellE5430 <rmpgit@gmail.com>
2020-08-25 20:40:07 +0200

    dodanie strony z wyborem

---
+++ wybor-header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <title> 2020 IV Agro Targi Wschód </title>
+    <!-- test rmpgit -->
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+
+<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="./dist/css/slick.css">
+    <link rel="stylesheet" href="./dist/css/simplelightbox.min.css">
+    <link rel="stylesheet" href="./dist/css/main.min.css">
+    <link rel="stylesheet" href="./dist/css/style.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+    <script src="./dist/js/slick.min.js"></script>
+<!--    <script src="dist/js/scripts.min.js"></script>-->
+    <script src="./dist/js/simple-lightbox.min.js"></script>
+    <script src="./src/js/main.js"></script>
+
+
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+<!--     <script src="https://cdnjs.cloudflare.com/ajax/lib/s/simplelightbox/1.11.1/simple-lightbox.js"></script>-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/1.11.1/simplelightbox.css">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-98798397-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-98798397-1');
+    </script>
+
+
+    <!--<script type="text/javascript" src="../../dist/js/script.js"></script>-->
+
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ wybor.php
@@ -0,0 +1,44 @@
+<?php require_once('wybor-header.php') ?>
+
+
+
+
+ <div class="assosiates">
+        <div class="container2">
+            <div class="row">
+                <div class="col-12 col-md-4">
+                    <a href="https://agrotargiwschod.pl" class="item">
+                        <div class="thumb">
+                            <img src="./src/assets/img/_update/agrotargi.png" alt="">
+                        </div>
+                        
+                    </a>
+                </div>  
+                <div class="col-12 col-md-4">
+                    <a href="https://targiwoyciechowskie.pl" class="item">
+                        <div class="thumb">
+                            <img src="./src/assets/img/_update/targiwoyciechowskie.png" alt="">
+                        </div>
+                        
+                    </a>
+                </div>  
+                <div class="col-12 col-md-4">
+                    <a href="https://gieldawschod.pl" class="item">
+                        <div class="thumb">
+                            <img src="./src/assets/img/_update/gieldawschod.png" alt="">
+                        </div>
+                        
+                    </a>
+                </div>    
+            </div>
+        </div>
+    </div>    
+
+<div class="col-md-6 col-lg-12 col-xl-auto d-flex align-items-center justify-content-center">
+                    <small class="mr-3">ORGANIZATOR:</small>
+                    <img src="./src/assets/img/_update/sawaexpo.png" alt="SAWA EXPO">
+</div>
+
+
+<?php require_once('footer.php') ?>
+
